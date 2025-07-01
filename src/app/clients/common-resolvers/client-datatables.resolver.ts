38a6a7bb8762a7b449792de8f968052901477812
Hassan Mehdi <98.hassanmehdi@gmail.com>
2025-07-01 15:42:03 +0400

    feat(CRED-91): Datatable view labels to represent meaningful names (#14)
    * feat: changed datatable label to be readable
    
    * CRED-91: Update name for loan datatables
    
    * CRED-91: Formats code with prettier
    
    ---------
    
    Co-authored-by: Hamza Farooq <hamza.farooq@crediblex.io>

--- src/app/clients/common-resolvers/client-datatables.resolver.ts
+++ src/app/clients/common-resolvers/client-datatables.resolver.ts
@@ -7,6 +7,7 @@ import { Observable } from 'rxjs';
 
 /** Custom Services */
 import { ClientsService } from '../clients.service';
+import { map } from 'rxjs/operators';
 
 /**
  * Client datatables resolver.
@@ -18,11 +19,41 @@ export class ClientDatatablesResolver implements Resolve<Object> {
    */
   constructor(private clientsService: ClientsService) {}
 
+  private convertSnakeToPascalCase(snakeCase: string): string {
+    return snakeCase
+      .split('_')
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(' ');
+  }
+
+  private transformDatatableLabel(datatable: any): string {
+    const separator = '_';
+    // format: dt_client_data_table_name
+    const [
+      dt,
+      entityName,
+      ...actualTableNameParts
+    ] = datatable.registeredTableName.split(separator);
+
+    // transform snake case "actualTableName" to pascal case
+    const tableName = actualTableNameParts.join(separator);
+    return this.convertSnakeToPascalCase(tableName);
+  }
+
   /**
    * Returns the Client datatables.
    * @returns {Observable<any>}
    */
   resolve(): Observable<any> {
-    return this.clientsService.getClientDatatables();
+    return this.clientsService.getClientDatatables().pipe(
+      map((data: any) => {
+        return data.map((datatable: any) => {
+          return {
+            ...datatable,
+            viewLabel: this.transformDatatableLabel(datatable)
+          };
+        });
+      })
+    );
   }
 }
--- src/app/loans/common-resolvers/loan-datatables.resolver.ts
+++ src/app/loans/common-resolvers/loan-datatables.resolver.ts
@@ -4,6 +4,7 @@ import { Resolve } from '@angular/router';
 
 /** rxjs Imports */
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 /** Custom Services */
 import { LoansService } from '../loans.service';
@@ -18,11 +19,41 @@ export class LoanDatatablesResolver implements Resolve<Object> {
    */
   constructor(private loansService: LoansService) {}
 
+  private convertSnakeToPascalCase(snakeCase: string): string {
+    return snakeCase
+      .split('_')
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(' ');
+  }
+
+  private transformDatatableLabel(datatable: any): string {
+    const separator = '_';
+    // format: dt_loan_data_table_name
+    const [
+      dt,
+      entityName,
+      ...actualTableNameParts
+    ] = datatable.registeredTableName.split(separator);
+
+    // transform snake case "actualTableName" to pascal case
+    const tableName = actualTableNameParts.join(separator);
+    return this.convertSnakeToPascalCase(tableName);
+  }
+
   /**
    * Returns the loan datatables.
    * @returns {Observable<any>}
    */
   resolve(): Observable<any> {
-    return this.loansService.getLoanDataTables();
+    return this.loansService.getLoanDataTables().pipe(
+      map((dataTables: any) => {
+        return dataTables.map((datatable: any) => {
+          return {
+            ...datatable,
+            label: this.transformDatatableLabel(datatable)
+          };
+        });
+      })
+    );
   }
 }
